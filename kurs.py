f8db5f339f064280c1fa7e902ba7fa826fd4aeb5
shohruh223 <shohruh.abd2223@gmail.com>
2023-01-19 21:40:02 +0500

    Initial commit

---
+++ kurs.py
@@ -0,0 +1,35 @@
+# exchangerate-api.com
+import requests
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+from urils.util import BOT_TOKEN, API_KEY
+
+API_TOKEN = BOT_TOKEN
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+API_KEY = API_KEY
+currency='USD'
+url=f"https://v6.exchangerate-api.com/v6/{API_KEY}/pair/{currency}/UZS"
+
+response = requests.get(url)
+jsondata = response.json()
+kurs = response.json()['conversion_rate']
+
+
+@dp.message_handler(commands=['dollar'])
+async def send_wiki(message: types.Message):
+    try:
+        text = f"Bugungi kurs: 1AQSH dollar = {kurs} so'm"
+        await message.answer(text)
+    except:
+        await message.answer("noto'g'ri urunish")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
